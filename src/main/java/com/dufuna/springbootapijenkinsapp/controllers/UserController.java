d5200334fa11ffa5145348a59c908f1b8528b4f8
Jeffrytech <rowlandjeffry@gmail.com>
2023-03-08 11:21:25 +0100

    I am the java Lord

--- src/main/java/com/dufuna/springbootapijenkinsapp/controllers/UserController.java
+++ src/main/java/com/dufuna/springbootapijenkinsapp/controllers/UserController.java
@@ -12,6 +12,6 @@ public class UserController {
 	@GetMapping("/")
 	public ResponseEntity<String> getAllUsers()
 	{
-		return ResponseEntity.ok("ThankGod I made it");
+		return ResponseEntity.ok("I am the Java Lord !!!!");
 	}
 }
